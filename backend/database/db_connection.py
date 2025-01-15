17e3b57ce726abe0f71783aba1c4c25d1f3933e0
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2025-01-14 22:12:36 -0300

    Revert "Configurando o Banco de Dados"
    This reverts commit 283eabfe383fc49fa3afce5823ac964fb3d783bd.

--- backend/database/db_connection.py
+++
@@ -1,9 +0,0 @@
-from decouple import config
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-
-DB_URl = config('DB_URL')
-
-engine = create_engine(DB_URl, pool_pre_ping=True)
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-
-app = FastAPI()
-
-@app.get('/test')
-def teste_endpoint():
-    return {'msg': 'sucess'}
-
