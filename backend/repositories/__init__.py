b9b02c33ae62ee6566a2bc4e5077591ebe4ffb9c
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2025-10-06 02:54:04 -0300

    feat(Repo): modulariza a pasta de repositórios

---
+++ backend/repositories/__init__.py
@@ -0,0 +1,5 @@
+from .auth_user import UserUseCases
+from .empresa_repo import EmpresaRepositorio
+from .funcionario_repo import FuncionarioRepo
+from .ponto_repo import PontoRepo
+from .relogio_repo import RelogioRepository
