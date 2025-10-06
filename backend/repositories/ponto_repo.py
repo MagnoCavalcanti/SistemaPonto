9481b435f2460d9c5292a7ccbbc8de132d505dab
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2025-10-06 02:54:47 -0300

    feat(Repo): adiciona o método de bulk_insert ao repositório de registros

--- backend/repositories/ponto_repo.py
+++ backend/repositories/ponto_repo.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from fastapi import status, HTTPException
+from pydantic import ValidationError
 
 
 import os
@@ -32,4 +33,30 @@ class PontoRepo:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail='Dados inválidos!'
+            )
+        
+    def sync_registers(self, registers: list[dict]):
+        registers_list: list[Registro_models] = []
+        
+        try:
+            for register in registers:
+                register_on_db = Registro_models(**register)
+
+                registers_list.append(register_on_db)
+            
+            self.db.add_all(registers_list)
+            self.db.commit()
+        except ValidationError as e:
+            print(e)
+        except IntegrityError as e:
+            self.db.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Erro de integridade: {e}"
+            )
+        except Exception as e:
+            self.db.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=str(e)
             )
