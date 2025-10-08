b366c29f84a7b9863fe5ff618910357a9dd4e06c
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2025-10-08 05:39:05 -0300

    refactor(Services): adiciona o arquivo com funções úteis

---
+++ backend/services/__init__.py
@@ -0,0 +1,3 @@
+from .genPDF import PDFGenerator_Repo
+from .depends import get_current_user, verificar_empresa
+from .managerWS import ConnectionManager
---
+++ backend/services/depends.py
@@ -0,0 +1,38 @@
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from jose import jwt
+from fastapi.security import OAuth2PasswordBearer
+
+import os
+import sys
+
+absolut_path = os.path.abspath(os.curdir)
+sys.path.insert(0, absolut_path)
+
+from backend.repositories import UserUseCases, EmpresaRepositorio
+from backend.database.seed_data import get_db_session
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://127.0.0.1:8000/login")  # Define o esquema de autenticação
+
+def verificar_empresa(empresa: str, db: Session = Depends(get_db_session)):
+    empresa_repo = EmpresaRepositorio(db)
+    empresa = empresa_repo.search_empresa(empresa_nome=empresa)
+    return empresa.id
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db_session)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        # Decodifica o token
+        uc = UserUseCases(dbsession=db)
+        payload = uc.verify_token(token)
+        
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
+    except jwt.JWTError:
+        raise credentials_exception
